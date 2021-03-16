4201ac6429b12a15fa3701e231ee5e17344ac880
LorenzoTurco <turco.lorenzo.27@gmail.com>
2021-03-16 17:42:05 +0000

    Revert "Structure4.0"
    This reverts commit c5929dd0751598bc16be511d4dd8082e693995a1.

---
+++ HomeComponents/Favorites.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './HomeComponents.css';
+
+function Favorites(){
+    return(
+
+        <div className="FavoritesButton">
+            <a href="/Favorites" target="_blank">
+	        <button>Click me!</button>
+            </a>
+        </div>
+
+
+    )
+}
+
+export default Favorites
---
+++ HomeComponents/Settings.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './HomeComponents.css';
+
+function Settings(){
+    return(
+
+        <div className="SettingsButton">
+            <a href="/Settings" target="_blank">
+	        <button>Click me!</button>
+            </a>
+        </div>
+
+
+    )
+}
+
+export default Settings
