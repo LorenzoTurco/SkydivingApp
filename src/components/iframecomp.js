d5e1d5bf369984f1c4192da20b58c4dd4533d192
WeifengSun1223 <60145712+WeifengSun1223@users.noreply.github.com>
2021-03-24 23:13:42 +0000

    Update iframecomp.js
    Changed the layout of rainfall map

--- src/components/iframecomp.js
+++ src/components/iframecomp.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import BackButton from './BackButton.js'
+import './iframecomp.css'
 export default class IframeComponent extends React.Component {
   render() {
     return (
       <div>
-        <iframe src={this.props.src} title="mymap" height={this.props.height} width={this.props.width} className="fullheight"/>
+        <iframe src={this.props.src} title="mymap" height={this.props.height} className="fullheight"/>
+        <header>
+          <BackButton />
+        </header>
       </div>
     )
   }
 }
+
