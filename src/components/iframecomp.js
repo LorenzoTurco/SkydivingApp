5fd74bf58df919d988de57a8ca031184afbf0031
WeifengSun1223 <60145712+WeifengSun1223@users.noreply.github.com>
2021-03-25 01:16:28 +0000

    Update iframecomp.js

--- src/components/iframecomp.js
+++ src/components/iframecomp.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import BackButton from './BackButton.js'
-import './iframecomp.css'
+import BackButton1 from './BackButton_1.js'
 export default class IframeComponent extends React.Component {
   render() {
     return (
       <div>
         <iframe src={this.props.src} title="mymap" height={this.props.height} className="fullheight"/>
-        <header>
-          <BackButton />
-        </header>
+        <BackButton1/>
       </div>
     )
   }
