e78c097e8a3a2d10c15a4a27a8d7454c59d12a64
WeifengSun1223 <60145712+WeifengSun1223@users.noreply.github.com>
2021-03-25 01:18:08 +0000

    Add files via upload

---
+++ src/components/BackButton_1.js
@@ -0,0 +1,27 @@
+import { useHistory } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import "./Navigation.css"
+
+var style = {height:"4.5%", width:"100%", textAlign:"center",
+fontSize:"20px", fontWeight:"bold", backgroundColor:"black", boxShadow:"0px -10px black", padding:"0 0 3px 0"}
+
+const BackButton_1 = () => {
+    const history = useHistory()
+
+    const onClick = () => {
+        <Link to={history.push({
+            pathname:"/"
+        })} className="text"></Link>
+    }
+
+    return (
+        <input
+            id ="back"
+            placeholder={"Back to homepage"}
+            style={style}
+            onClick={onClick}
+            alt="back"
+         />
+    )
+}
+export default BackButton_1
