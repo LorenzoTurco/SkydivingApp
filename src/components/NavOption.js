31c27621602e645cd92fd00e1db1695ffe5ce2d3
WeifengSun1223 <60145712+WeifengSun1223@users.noreply.github.com>
2021-03-27 01:35:42 +0000

    Update NavOption.js

--- src/components/NavOption.js
+++ src/components/NavOption.js
@@ -1,14 +1,16 @@
 import { Link } from 'react-router-dom'
-
+import "./Navigation.css"
 const NavOption = ({ url, text, imgsrc }) => {
     return (
-        <p>
-            <img
+        <div className="Nav">
+            <img 
                 src={imgsrc} alt=""
                 style={{ maxWidth: "50px" }}
             />
-            <Link to={`/${url}`}>{text}</Link>
-        </p>
+            <div style={{display:"inline-block", verticalAlign:"top"}}>
+                <Link to={`/${url}`} className="text">{text}</Link>
+            </div>
+        </div>
     )
 }
 
