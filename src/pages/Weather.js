dd7cc54b24697dcc84a40b65c722239bec95d863
WeifengSun1223 <60145712+WeifengSun1223@users.noreply.github.com>
2021-03-27 00:59:25 +0000

    Style the weather page

--- src/pages/Weather.js
+++ src/pages/Weather.js
@@ -1,9 +1,11 @@
+  
 import { useState, useEffect } from 'react'
 import { useHistory, useParams } from 'react-router-dom'
 import Searchbar from '../components/Searchbar'
 import DisplayData from '../components/DisplayData'
 import AddFav from '../components/AddFav'
 import BackButton from '../components/BackButton'
+import LocationIcon from '../assets/location.png'
 
 const Weather = () => {
     const history = useHistory()
@@ -17,27 +19,35 @@ const Weather = () => {
 
     return (
         <div>
-            <Searchbar />
             <BackButton />
-            <h1> {parseInt(data.list[0].main.temp)}&#176; </h1>
-            <h1> 
-                {parseInt(data.list[0].wind.speed)}mph <br/>
-                {/* Gets the wind direction using some calculation */}
-                {directions[Math.round(((data.list[0].wind.deg %= 360) < 0 ? data.list[0].wind.deg + 360 : data.list[0].wind.deg) / 45) % 8]}
-            </h1>
-            <h2>
-                {data.list[0].weather[0].main} <br/>
-                {/* More specific description of the main weather focus.
-                    e.g. if 'Clouds', the description could say 'few clouds' all the way to 'overcase clouds' */}
-                {data.list[0].weather[0].description} <br/>
-                <img src={`http://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@2x.png`} />
-            </h2>
-            <h3> {data.city.name}, {data.city.country}</h3>
             {/* AddFav to create a bookmark icon that favourites saved locations */}
             <AddFav
                 locationName={history.location.state.city.name}
                 countryName={history.location.state.city.country}
             />
+            <Searchbar />
+            <div className="generalInfo">
+                <p className="currentTemp"> {parseInt(data.list[0].main.temp)}&#176; </p>
+                <p className="location"> 
+                    {data.city.name}, {data.city.country}
+                    <input
+                        type="image"
+                        src={LocationIcon}
+                        style={{width:"20px", height:"25px", marginRight:"27vw", paddingTop:"5px"}}
+                        alt="location"
+                    />
+                </p>
+                <img src={`http://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@2x.png`} style={{height:"70px", 
+                    width:"70px",float:"right", marginTop:"7vh", marginRight:"25vw"}}/>
+                <p className="weatherMain">
+                    {data.list[0].weather[0].main} <br/>
+                </p>
+                {/* More specific description of the main weather focus.
+                    e.g. if 'Clouds', the description could say 'few clouds' all the way to 'overcase clouds' */}
+                <p className="weatherDescription">
+                    <p>{data.list[0].weather[0].description}</p>
+                </p>
+            </div>
             {/* Display data for showing the forecast using a swiper */}
             <DisplayData data={data} />
         </div>
