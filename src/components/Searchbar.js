69e3f535e254611e24028d9cd423f76dec74e062
LorenzoTurco <turco.lorenzo.27@gmail.com>
2021-03-29 00:08:11 +0100

    API change

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,60 +1,64 @@
-import SearchIcon from '../assets/search.png'
-import { useState } from 'react'
-import { useHistory } from 'react-router-dom'
+import SearchIcon from "../assets/search.png";
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
 
-const API_KEY = process.env.REACT_APP_API_KEY;
+const API_KEY = "16c48eb412d175d398c25a1054709818";
 
 // Searchbar for finding locations and then passing data to the weather page upon succesfully calling the API.
 const Searchbar = () => {
-    // Use input state
-    const [location, setLocation] = useState('')
+  // Use input state
+  const [location, setLocation] = useState("");
 
-    // Allows use of history to push and redirect.
-    const history = useHistory()
+  // Allows use of history to push and redirect.
+  const history = useHistory();
 
-    // When user submits searchbar
-    // Call API, if succesful redirect.
-    // If non-successful code then reset field and stay.
-    const onSubmit = async (e) => {
-        e.preventDefault()
-        if(!location) {
-            return
-        }
-        const tempData = await getLocationWeather(location)
-        if(tempData.cod==="200"){
-            history.push({
-                pathname: '/weatherpage/'+tempData.city.name,
-                state: tempData
-            })
-        } else {
-            setLocation('')
-        }
+  // When user submits searchbar
+  // Call API, if succesful redirect.
+  // If non-successful code then reset field and stay.
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    if (!location) {
+      return;
     }
-
-    // API call
-    const getLocationWeather = async (location) => {
-        let url = `https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=${API_KEY}&units=${localStorage.getItem('settings')}`
-        const result = await fetch(url)
-        const data = await result.json()
-        return data
+    const tempData = await getLocationWeather(location);
+    if (tempData.cod === "200") {
+      history.push({
+        pathname: "/weatherpage/" + tempData.city.name,
+        state: tempData,
+      });
+    } else {
+      setLocation("");
     }
+  };
 
-    return (
-        <form className="searchbar" onSubmit={onSubmit}>
-            <input className = "search"
-                style={{}}
-                value={location}
-                placeholder={"Search location"}
-                onChange={(e) => setLocation(e.target.value)}
-            />
-            <input className="leftimg"
-                type="image"
-                src={SearchIcon}
-                style={{ maxWidth: "20px" }}
-                alt="Submit"
-            />
-        </form>
-    )
-}
+  // API call
+  const getLocationWeather = async (location) => {
+    let url = `https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=${API_KEY}&units=${localStorage.getItem(
+      "settings"
+    )}`;
+    const result = await fetch(url);
+    const data = await result.json();
+    return data;
+  };
 
-export default Searchbar
+  return (
+    <form className="searchbar" onSubmit={onSubmit}>
+      <input
+        className="search"
+        style={{}}
+        value={location}
+        placeholder={"Search location"}
+        onChange={(e) => setLocation(e.target.value)}
+      />
+      <input
+        className="leftimg"
+        type="image"
+        src={SearchIcon}
+        style={{ maxWidth: "20px" }}
+        alt="Submit"
+      />
+    </form>
+  );
+};
+
+export default Searchbar;
