a4e2dfe1756eaa706444870a365ae8ac5e2af553
LorenzoTurco <turco.lorenzo.27@gmail.com>
2021-03-28 03:07:54 +0100

    indent fix

--- src/pages/Favourites.js
+++ src/pages/Favourites.js
@@ -1,120 +1,101 @@
-import { useState,useEffect } from 'react'
-import { useHistory } from 'react-router-dom'
-import './Favourites.css';
-import BackButton from '../components/BackButton'
+import { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import "./Favourites.css";
+import BackButton from "../components/BackButton";
 
 const API_KEY = process.env.REACT_APP_API_KEY;
-console.log(API_KEY)
+console.log(API_KEY);
 
-  var favouritesArray = getFavourites() 
+var favouritesArray = getFavourites();
 
-  function getFavourites(){ //returns an array of JSON objects marked as favorites from localstorage
+function getFavourites() {
+  //returns an array of JSON objects marked as favorites from localstorage
 
-    favouritesArray = JSON.parse(localStorage.getItem('favourites'))
-    return favouritesArray
+  favouritesArray = JSON.parse(localStorage.getItem("favourites"));
+  return favouritesArray;
 }
 
-
-
 const Favourites = () => {
+  const [location, setLocation] = useState({});
+  const history = useHistory();
 
-    const [location, setLocation] = useState({})
-    const history = useHistory()
+  //redirects user to the relevant weather page based on form submission.
+  const onSubmit = async (e) => {
+    e.preventDefault();
 
-    //redirects user to the relevant weather page based on form submission.
-    const onSubmit = async (e) => {
-        e.preventDefault()
-
-        const tempData = await getLocationWeather(location)
-        if(tempData.cod==="200"){
-            history.push({
-                pathname: '/weatherpage/'+tempData.city.name,
-                state: tempData
-            })
-        }
+    const tempData = await getLocationWeather(location);
+    if (tempData.cod === "200") {
+      history.push({
+        pathname: "/weatherpage/" + tempData.city.name,
+        state: tempData,
+      });
     }
+  };
 
-    //Data is fetched
-    const getLocationWeather = async (location) => {
+  //Data is fetched
+  const getLocationWeather = async (location) => {
+    let url = `https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=${API_KEY}&units=${localStorage.getItem(
+      "settings"
+    )}`;
+    const result = await fetch(url);
+    const data = await result.json();
+    return data;
+  };
 
-        let url = `https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=${API_KEY}&units=${localStorage.getItem('settings')}`
-        const result = await fetch(url)
-        const data = await result.json()
-        return data
-    }
+  const buttonNames = getLocationMap();
 
+  function getLocationMap() {
+    // creates a new favorite button for each JSON object in the favouritesArray
 
-
-        const buttonNames = getLocationMap()
-
-
-        function getLocationMap(){ // creates a new favorite button for each JSON object in the favouritesArray
-        
-        if(favouritesArray){
-            return(
-            
-        (favouritesArray.favourite).map(favourite => 
-        
-            <div className="wrapper">
-        <button className="button" 
-        
-        value={favourite.name +"," + favourite.country}
-        onClick={(e) => setLocation(e.target.value)}
-        
-        >
-            
-        {favourite.name + ", "+favourite.country }
-        </button>
+    if (favouritesArray) {
+      return favouritesArray.favourite.map((favourite) => (
+        <div className="wrapper">
+          <button
+            className="button"
+            value={favourite.name + "," + favourite.country}
+            onClick={(e) => setLocation(e.target.value)}
+          >
+            {favourite.name + ", " + favourite.country}
+          </button>
         </div>
-        )
-        )}else{
+      ));
+    } else {
+      console.log("empty");
+    }
+  }
 
-            console.log("empty")
-            
-        }}
+  function clear() {
+    //removes all favourites locations
 
+    localStorage.removeItem("favourites");
+    window.location.reload();
+  }
 
-        function clear(){ //removes all favourites locations
+  function refresh() {
+    //updates the page
+    window.location.reload();
+  }
 
-            localStorage.removeItem('favourites')
-            window.location.reload()
-        }
+  return (
+    <div className="background">
+      <BackButton />
 
-        function refresh(){ //updates the page
-            window.location.reload()
+      <div className="container">
+        <button className="refresh" onClick={() => refresh()}>
+          Refresh
+        </button>
+      </div>
 
-        }
-    
-    return(
+      <div className="container">
+        <button className="clear" onClick={() => clear()}>
+          Clear
+        </button>
+      </div>
+      <form onSubmit={onSubmit}>
+        <div>{buttonNames}</div>
+      </form>
+    </div>
+  );
+};
 
-            <div className="background">
-
-            <BackButton/>
-                    
-                <div className="container">
-                    <button className="refresh"onClick={() => refresh()}>Refresh</button>
-                </div>
-
-                <div className="container">
-               <button className="clear"onClick={() => clear()}>Clear</button>
-                </div>
-                <form onSubmit={onSubmit}> 
-
-                    
-                <div>
-                    {buttonNames}
-                </div>
-                     
-                    
-
-                   
-                </form>
-
-
-            </div>
-
-
-    )}
-
-
-    export default Favourites
+export default Favourites;
