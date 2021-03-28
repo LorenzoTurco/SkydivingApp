5e09c739701655d04ee8637559ea71a60e6c2fad
LorenzoTurco <turco.lorenzo.27@gmail.com>
2021-03-28 19:34:04 +0100

    recGear background + remove recLoc

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import { useEffect } from 'react'
-import Home from './pages/Home'
-import Rainfall from './pages/Rainfall'
-import Favourites from './pages/Favourites'
-import RecGear from './pages/RecGear'
-import RecLocation from './pages/RecLocation'
-import Settings from './pages/Settings'
-import Weather from './pages/Weather'
-import Error from './pages/Error'
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { useEffect } from "react";
+import Home from "./pages/Home";
+import Rainfall from "./pages/Rainfall";
+import Favourites from "./pages/Favourites";
+import RecGear from "./pages/RecGear";
+import Settings from "./pages/Settings";
+import Weather from "./pages/Weather";
+import Error from "./pages/Error";
 
 function App() {
-
   // Check that settings in the local storage exist, if not set to default metric.
   useEffect(() => {
-    let currentSettings = localStorage.getItem('settings')
+    let currentSettings = localStorage.getItem("settings");
     if (currentSettings === null) {
-      localStorage.setItem('settings','Metric')
+      localStorage.setItem("settings", "Metric");
     }
-
-  },[])
+  }, []);
 
   // Allows for client side routing, different routes load different pages.
   return (
@@ -35,9 +31,7 @@ function App() {
         <Route path="/favourites">
           <Favourites />
         </Route>
-        <Route path="/recommendedlocation">
-          <RecLocation />
-        </Route>
+
         <Route path="/settings">
           <Settings />
         </Route>
